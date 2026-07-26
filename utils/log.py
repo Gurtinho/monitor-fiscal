b27a9f00b7d696434d4041f700878ddc3c5bc5e2
gurtinho <gustavolitter@gmail.com>
2026-07-26 16:32:39 -0300

    fix()

--- utils/log.py
+++ utils/log.py
@@ -1,11 +1,9 @@
 import logging
 
-# Configuração básica
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
-        # logging.FileHandler("monitor_fiscal.log"), # Salva num arquivo
-        logging.StreamHandler() # Mostra no terminal
+        logging.FileHandler("monitor_fiscal.log"), # Salva num arquivo
     ]
 )
